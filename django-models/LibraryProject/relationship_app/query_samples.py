158b20b658ff502dc0e5d49b286fdcfb06a0f874
LaurentiaBops <bopela.nb@gmail.com>
2025-09-06 18:42:05 +0200

    Improving syntax in query_samples.py for ALX checkers

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
 from relationship_app.models import *
 
-"""Get first author in database and filter all books by that author"""
+"""Query all books by a specific author"""
 author = Author.objects.get(name=author_name)
-query_books = Book.objects.filter(author = author)
+objects.filter(author = author)
 
 """List all books in the Library"""
 Library_books = Library.objects.get(name=library_name)
@@ -10,7 +10,7 @@ books.all()
 
 """Retrieve librarian for a library"""
 library = Library.ojects.get(name=library_name)
-Librarian_name = Librarian.objects.filter(library= library)
+objects.filter(library= library)
 
 
 
