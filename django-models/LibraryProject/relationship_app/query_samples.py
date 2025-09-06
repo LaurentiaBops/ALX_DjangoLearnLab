b7671ab22d68dd519c17fc894018d95e19b70f43
LaurentiaBops <bopela.nb@gmail.com>
2025-09-06 18:37:43 +0200

    Improving the syntax in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,16 +1,16 @@
 from relationship_app.models import *
 
 """Get first author in database and filter all books by that author"""
-first_author = Author.objects.first()
-query_books = Book.objects.filter(author = first_author)
+author = Author.objects.get(name=author_name)
+query_books = Book.objects.filter(author = author)
 
 """List all books in the Library"""
 Library_books = Library.objects.get(name=library_name)
 books.all()
 
 """Retrieve librarian for a library"""
-Library_name = Library.ojects.get()
-Librarian_name = Librarian.objects.filter(library= Library_name)
+library = Library.ojects.get(name=library_name)
+Librarian_name = Librarian.objects.filter(library= library)
 
 
 
