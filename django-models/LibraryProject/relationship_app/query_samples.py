d9e321670065f63c2c35c4643c701d995ed32c55
LaurentiaBops <bopela.nb@gmail.com>
2025-09-06 18:44:03 +0200

    Syntax

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,7 +2,7 @@ from relationship_app.models import *
 
 """Query all books by a specific author"""
 author = Author.objects.get(name=author_name)
-objects.filter(author = author)
+objects.filter(author=author)
 
 """List all books in the Library"""
 Library_books = Library.objects.get(name=library_name)
