325d504267d3a5328828948eee53fe1d3712d49f
LaurentiaBops <bopela.nb@gmail.com>
2025-09-06 23:53:57 +0200

    Adding template name reference for logoutview

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     path('login/',LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/',LogoutView.as_view(), name='logout'),
+    path('logout/',LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/'views.register, name='register'),
     path("",views.index, name="index"),
 ]
